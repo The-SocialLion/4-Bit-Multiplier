f1010326093eae9962cda7d15aec7356386a6f94
Vishal Balaji Sivaraman <64977199+The-SocialLion@users.noreply.github.com>
2021-03-26 16:14:21 +0530

    Add files via upload

---
+++ mult/isim/work/mult/mult.h
@@ -0,0 +1,39 @@
+////////////////////////////////////////////////////////////////////////////////
+//   ____  ____  
+//  /   /\/   /  
+// /___/  \  /   
+// \   \   \/    
+//  \   \        Copyright (c) 2003-2004 Xilinx, Inc.
+//  /   /        All Right Reserved. 
+// /___/   /\   
+// \   \  /  \  
+//  \___\/\___\ 
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef H_workMmult_H
+#define H_workMmult_H
+
+#ifdef _MSC_VER
+#pragma warning(disable: 4355)
+#endif
+
+#ifdef __MINGW32__
+#include "xsimMinGW.h"
+#else
+#include "xsim.h"
+#endif
+
+class workMmult : public HSim__s5{
+public: 
+    workMmult(const char *instname);
+    ~workMmult();
+    void setDefparam();
+    void constructObject();
+    void moduleInstantiate(HSimConfigDecl *cfg);
+    void connectSigs();
+    void reset();
+    virtual void archImplement();
+    HSim__s1 us[10];
+};
+
+#endif
---
+++ mult/mult.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:40:44 03/26/2021 
+// Design Name: 
+// Module Name:    mult 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mult(a, b, p);
+    input [3:0]a;
+    input [3:0]b;
+	 wire [3:0]w0;
+	 wire [4:0]w1;
+	 wire [5:0]w2;
+	 wire [6:0]w3;
+	 wire [7:0] s1,s2,s3;
+    output [7:0]p;
+	 assign w0={4{a[0]}}&b[3:0];
+	 assign w1={4{a[1]}}&b[3:0];
+	 assign w2={4{a[2]}}&b[3:0];
+	 assign w3={4{a[3]}}&b[3:0];
+	 assign s1=w0+(w1<<1);
+	 assign s2=s1+(w2<<2);
+	 assign s3=s2+(w3<<3);
+	 assign p=s3;
+endmodule
